588678fca90e499e05beb83a33ae256f09104671
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-04-28 06:30:03 +0200

    One

--- index.js
+++ index.js
@@ -15,19 +15,27 @@ try {
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((res) => res.text())
   .then((body) => {
-    // Fetch first 10 img-contents from img URL and save them
-    for (let i = 0; i < 10; i++) {
-      const currentImg = $('img', body)[i].attribs.src;
+    const imgUrls = $('img', body)
+      .slice(0, 10)
+      .map((i, el) => $(el).attr('src'));
 
-      fetch(currentImg).then((res) => {
-        const path =
-          './memes/' +
-          currentImg.split('?')[0].split('/').slice(4).join('_');
+    const downloadPromises = imgUrls.map((url) => {
+      const path = './memes/' + url.split('?')[0].split('/').slice(4).join('_');
 
+      return fetch(url).then((res) => {
         const dest = fs.createWriteStream(path);
         res.body.pipe(dest);
       });
-    }
+    });
 
-    console.log('Images downloaded successfully!');
+    Promise.all(downloadPromises)
+      .then(() => {
+        console.log('Images downloaded successfully!');
+      })
+      .catch((err) => {
+        console.error('Error downloading images:', err);
+      });
+  })
+  .catch((err) => {
+    console.error('Error fetching HTML:', err);
   });
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -5,3 +5,4 @@ const config = {
 };
 
 module.exports = config;
+
